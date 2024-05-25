10c216e9cf893381767a37c449f63e4241cf5d19
Tarek Mohamed <72219734+tarekmohamedx@users.noreply.github.com>
2024-05-25 16:34:33 +0300

    Add files via upload

--- src/data/teamMembers.ts
+++ src/data/teamMembers.ts
@@ -1,6 +1,10 @@
 import { v4 as uuidV4 } from "uuid";
 import member from "@/assests/members/member.webp";
-import member2 from "@/assests/members/member.png";
+import member2 from "@/assests/members/member2.webp";
+import member3 from "@/assests/members/member3.webp";
+import member4 from "@/assests/members/member4.webp";
+import member5 from "@/assests/members/member5.webp";
+
 
 type MemberType = {
   id: string;
@@ -18,8 +22,8 @@ type MemberType = {
 export const teamMembers: MemberType[] = [
   {
     id: uuidV4(),
-    name: "Mahmoud Elmorsy",
-    role: "Computer Vision-Ai",
+    name: "Ahmed Arafat",
+    role: "Flutter Developer",
     image: member.src,
     socials: {
       facebook: "https://www.facebook.com/mahmoud.elmorsy.7",
@@ -40,8 +44,41 @@ export const teamMembers: MemberType[] = [
   },
   {
     id: uuidV4(),
-    name: "Mahmoud Elmorsy",
-    role: "Computer Vision-Ai",
+    name: "Mohsen Essam",
+    role: "Data Engineer",
+    image: member3.src,
+    socials: {
+      facebook: "https://www.facebook.com/mahmoud.elmorsy.7",
+      instagram: "https://www.instagram.com/mahmoud.elmorsy.7",
+      twitter: "https://twitter.com/mahmoud_elmorsy",
+    },
+  },
+  {
+    id: uuidV4(),
+    name: "Hazem Elkhamisi",
+    role: "...",
+    image: member4.src,
+    socials: {
+      facebook: "https://www.facebook.com/mahmoud.elmorsy.7",
+      instagram: "https://www.instagram.com/mahmoud.elmorsy.7",
+      twitter: "https://twitter.com/mahmoud_elmorsy",
+    },
+  },
+  {
+    id: uuidV4(),
+    name: "Mahmoud Abdo",
+    role: "Backend",
+    image: member5.src,
+    socials: {
+      facebook: "https://www.facebook.com/mahmoud.elmorsy.7",
+      instagram: "https://www.instagram.com/mahmoud.elmorsy.7",
+      twitter: "https://twitter.com/mahmoud_elmorsy",
+    },
+  },
+  {
+    id: uuidV4(),
+    name: "Mahmoud Elsaeed",
+    role: "...",
     image: member.src,
     socials: {
       facebook: "https://www.facebook.com/mahmoud.elmorsy.7",
@@ -51,8 +88,8 @@ export const teamMembers: MemberType[] = [
   },
   {
     id: uuidV4(),
-    name: "Mahmoud Elmorsy",
-    role: "Computer Vision-Ai",
+    name: "Ahmed Foaad",
+    role: "...",
     image: member.src,
     socials: {
       facebook: "https://www.facebook.com/mahmoud.elmorsy.7",
@@ -62,41 +99,8 @@ export const teamMembers: MemberType[] = [
   },
   {
     id: uuidV4(),
-    name: "Mahmoud Elmorsy",
-    role: "Computer Vision-Ai",
-    image: member.src,
-    socials: {
-      facebook: "https://www.facebook.com/mahmoud.elmorsy.7",
-      instagram: "https://www.instagram.com/mahmoud.elmorsy.7",
-      twitter: "https://twitter.com/mahmoud_elmorsy",
-    },
-  },
-  {
-    id: uuidV4(),
-    name: "Mahmoud Elmorsy",
-    role: "Computer Vision-Ai",
-    image: member.src,
-    socials: {
-      facebook: "https://www.facebook.com/mahmoud.elmorsy.7",
-      instagram: "https://www.instagram.com/mahmoud.elmorsy.7",
-      twitter: "https://twitter.com/mahmoud_elmorsy",
-    },
-  },
-  {
-    id: uuidV4(),
-    name: "Mahmoud Elmorsy",
-    role: "Computer Vision-Ai",
-    image: member.src,
-    socials: {
-      facebook: "https://www.facebook.com/mahmoud.elmorsy.7",
-      instagram: "https://www.instagram.com/mahmoud.elmorsy.7",
-      twitter: "https://twitter.com/mahmoud_elmorsy",
-    },
-  },
-  {
-    id: uuidV4(),
-    name: "Mahmoud Elmorsy",
-    role: "Computer Vision-Ai",
+    name: "Tareq Mohamed",
+    role: "Junior ...",
     image: member.src,
     socials: {
       facebook: "https://www.facebook.com/mahmoud.elmorsy.7",
